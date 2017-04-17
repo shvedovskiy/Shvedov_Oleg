a7331db4a4b7596dc5d2ab6891d75d1a56ff9d11
Oleg Shvedov <www.goo5@me.com>
2017-04-17 20:22:17 +0300

    scss init

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-sass');
 const clean = require('gulp-clean');
 const rebaseUrls = require('gulp-css-rebase-urls');
+const minify = require('gulp-minify-css');
 
 gulp.task('images-clean-dist', () =>
     gulp.src('dist/images', { read: false })
@@ -20,20 +21,18 @@ gulp.task('images:watch', ['images'],  () =>
 );
 
 gulp.task('scss', () =>
-    gulp.src('src/css/**/main.scss')
+    gulp.src('src/scss/**/main.scss')
         .pipe(sourcemaps.init())
-        .pipe(autoprefixer({
-            browsers: ['> 1%'],
-            cascade: false
-        }))
         .pipe(sass().on('error', sass.logError))
         .pipe(rebaseUrls())
+        .pipe(autoprefixer())
+        .pipe(minify())
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('dist/css'))
 );
 
 gulp.task('scss:watch', ['scss'], () =>
-    gulp.watch('src/styles/**/*.scss', ['scss'])
+    gulp.watch('src/scss/**/*.scss', ['scss'])
 );
 
 gulp.task('build', ['scss', 'images']);
